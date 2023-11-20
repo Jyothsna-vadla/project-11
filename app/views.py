1de269daa269df67e884c6833e4cf694d9d0eaaf
Jyothsna-vadla <150165187+Jyothsna-vadla@users.noreply.github.com>
2023-11-20 20:48:59 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def jyo(request):
+    return render(request,'jyo.html')
+def satheesh(request):
+    return render(request,'satheesh.html')
+def Akhila(request):
+    return render(request,'Akhila.html')
+def jyo2(request):
+    return render(request,'jyo2.html')
